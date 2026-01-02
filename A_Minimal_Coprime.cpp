380259052f65fb2763a869f4929a2118b76967bd
shariar17 <shariarhossain23@gmail.com>
2026-01-02 22:14:21 +0600

    solved100

---
+++ A_Minimal_Coprime.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <iostream>
+#define ll long long
+#define newLine cout << "\n"
+#define pb push_back
+#define vi vector<int>
+#define Yes cout << "YES\n"
+#define No cout << "NO\n"
+#define array_input_int(a, o, n) \
+    for (int i = o; i < n; i++)  \
+    {                            \
+        cin >> (a[i]);           \
+    }
+
+void solve()
+{
+
+    int a, b;
+    cin >> a >> b;
+
+    if (a == 1 and b == 1)
+        cout << 1 << endl;
+    else
+        cout << max(a, b) - min(a, b) << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
