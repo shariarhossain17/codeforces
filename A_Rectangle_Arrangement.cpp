c38182ab6c03673beaeccb8a28c41584f26703bd
shariar17 <shariarhossain23@gmail.com>
2026-01-04 19:36:49 +0600

    solved another 800

---
+++ A_Rectangle_Arrangement.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <iostream>
+#define ll long long
+#define newLine cout << "\n"
+#define pb push_back
+#define vi vector<int>
+#define Yes cout << "YES\n"
+#define No cout << "NO\n"
+#define array_input_int(a, o, n) \
+    for (int i = o; i < n; i++)  \
+    {                            \
+        cin >> (a[i]);           \
+    }
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    int w = 0, h = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        int wi, hi;
+        cin >> wi >> hi;
+        w = max(w, wi);
+        h = max(h, hi);
+    }
+
+    cout << 2 * (w + h) << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
