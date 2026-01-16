563c80f980561a6c4b514e1891477283d43d2c21
shariar17 <shariarhossain23@gmail.com>
2026-01-16 19:42:20 +0600

    solved

---
+++ B_Game_of_Ball_Passing.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <iostream>
+#define ll long long
+#define newLine cout << "\n"
+#define pb push_back
+#define vi vector<int>
+#define Yes cout << "YES\n"
+#define No cout << "NO\n"
+#define array_input_int(a, o, n) \
+    for (int i = o; i < n; i++)  \
+    {                            \
+        cin >> (a[i]);           \
+    }
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    if (a.back() == 0)
+    {
+        cout << "0\n";
+        return;
+    }
+    ll sum = 0;
+    for (ll i = 0; i < n - 1; i++)
+    {
+        sum += a[i];
+    }
+    cout << max(1LL, a.back() - sum) << '\n';
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
