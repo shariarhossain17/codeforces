3e9ef37dc1c291bd101f0c03900a590bf6dad880
shariar17 <shariarhossain23@gmail.com>
2026-02-14 19:45:51 +0600

    solved

---
+++ A_Most_Unstable_Array.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <iostream>
+#define ll long long
+#define newLine cout << "\n"
+#define pb push_back
+#define vi vector<int>
+#define Yes cout << "YES\n"
+#define No cout << "NO\n"
+#define array_input_int(a, o, n) \
+    for (int i = o; i < n; i++)  \
+    {                            \
+        cin >> (a[i]);           \
+    }
+
+void solve()
+{
+
+    ll n, m;
+    cin >> n >> m;
+
+    if (n == 1)
+        cout << 0 << endl;
+    else if (n == 2)
+        cout << m << endl;
+
+    else
+        cout << m * 2 << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
