96d550ee0f94e18adddaeeeb11d8dcb739fe2b80
shariar17 <shariarhossain23@gmail.com>
2026-03-06 23:02:33 +0600

    solved

--- B_Gifts_Fixing.cpp
+++ B_Gifts_Fixing.cpp
@@ -19,21 +19,21 @@ void solve()
     int n;
     cin >> n;
 
-    vector<ll> a(n), b(n);
+    vector<ll> a(n), v(n);
 
     array_input_int(a, 0, n);
-    array_input_int(b, 0, n);
+    array_input_int(v, 0, n);
 
     ll ans = 0;
 
     ll mn1 = *min_element(a.begin(), a.end());
-    ll mn2 = *min_element(b.begin(), b.end());
+    ll mn2 = *min_element(v.begin(), v.end());
 
     for (int i = 0; i < n; i++)
     {
 
         ll c = a[i] - mn1;
-        ll d = b[i] - mn2;
+        ll d = v[i] - mn2;
 
         ans += max(c, d);
     }
