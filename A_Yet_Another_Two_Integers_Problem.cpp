5b1af52f9a22eea85aa8a4013a16577d9dc1ecf0
shariar17 <shariarhossain23@gmail.com>
2026-03-09 22:44:19 +0600

    solved

---
+++ A_Yet_Another_Two_Integers_Problem.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <iostream>
+#define ll long long
+#define newLine cout << "\n"
+#define pb push_back
+#define vi vector<int>
+#define Yes cout << "YES\n"
+#define No cout << "NO\n"
+#define array_input_int(a, o, n) \
+    for (int i = o; i < n; i++)  \
+    {                            \
+        cin >> (a[i]);           \
+    }
+
+void solve()
+{
+
+    int a, b;
+    cin >> a >> b;
+
+    int dif = abs(a - b);
+    if (a == b)
+        cout << 0 << endl;
+    else if (dif <= 10)
+        cout
+            << 1 << endl;
+    else
+        cout << (dif + 10 - 1) / 10 << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
